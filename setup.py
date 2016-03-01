3237e0204f9d8dd1da7c498e1e7b828dedda8ab5
Alejandro Frias <alejandro@counsyl.com>
2016-03-01 11:46:30 -0800

    setting up setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name='case_conversion',
-  packages=['case_conversion'],
+  name='case-conversion',
+  packages=['case-conversion'],
   version='1.0.0',
   description='convert between different types of cases',
   author='Alejandro Frias',
   author_email='joker454@gmail.com',
-  url='https://github.com/AlejandroFrias/case_conversion',
-  download_url='https://github.com/AlejandroFrias/case_conversion/tarball/1.0.0',
+  url='https://github.com/AlejandroFrias/case-conversion',
+  download_url='https://github.com/AlejandroFrias/case-conversion/tarball/1.0.0',
   keywords=['case', 'convert', 'camel', 'pascal', 'snake'],
   classifiers=[],
 )
