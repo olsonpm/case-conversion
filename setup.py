d6ec924f1e161291f1fd23cb5001408d6ee64800
Alejandro Frias <alejandro@counsyl.com>
2016-03-01 13:25:29 -0800

    bumping version for bug fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='case_conversion',
   packages=['case_conversion'],
-  version='1.0.0',
+  version='1.0.1',
   description='convert between different types of cases',
   license='MIT',
   author='Alejandro Frias',
   author_email='joker454@gmail.com',
   url='https://github.com/AlejandroFrias/case-conversion',
-  download_url='https://github.com/AlejandroFrias/case-conversion/tarball/1.0.0',
+  download_url='https://github.com/AlejandroFrias/case-conversion/tarball/1.0.1',
   keywords=['case', 'convert', 'camel', 'pascal', 'snake'],
   classifiers=[],
 )
